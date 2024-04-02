e698ceb04b1138bdfef353fe8de194c10dcf01e3
Maria Balos <mariabalos16@gmail.com>
2024-04-02 18:15:06 +0000

    Create the open_status and argrarse functions and add docstrings

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -25,16 +26,23 @@ def open_driver():
 
 
 def close_driver(driver):
-    """Close the Firefos driver.
+    """Close the Firefox driver.
 
     Args:
         driver (WebDriver): the driver instanced to be used in other function.
     """
-    print(type(driver))
     driver.quit()
 
 
 def login_linkedin(driver, username, password):
+    """This function will login the user with the provided username and password.
+
+    Args:
+        driver (WebDriver): the driver instanced to be used in other function.
+        username (str): The user's email for the LInkedIn profile.
+        password (str): The user's password for the LinkedIn profile.
+    """
+    # Go to the LInkedIn Login page
     driver.get("https://www.linkedin.com/login")
 
     # Accept cookies
@@ -60,6 +68,14 @@ def login_linkedin(driver, username, password):
 
 
 def is_work_status_open(driver):
+    """Checks if the LinkedIn status is open.
+
+    Args:
+        driver (WebDriver): the driver instanced to be used in other function.
+
+    Returns:
+        bool: Returns True if the LinkedIn status is OpenToWork and False if is not.
+    """
     # Access the profile page
     driver.get("https://www.linkedin.com/in/me/")
 
@@ -70,11 +86,101 @@ def is_work_status_open(driver):
     return "#OPEN_TO_WORK" in content
 
 
-def open_status():
-    pass
+def open_status(driver):
+    """This function will turn the status on, adding the frame OpenToWork frame.
+
+    Args:
+        driver (WebDriver): the driver instanced to be used in other function.
+    """
+
+    # Go to the profile page
+    driver.get("https://www.linkedin.com/in/me/")
+
+    # Open the image menu
+    photo_btn_class = "profile-photo-edit__edit-btn"
+    photo_btn = driver.find_element(by=By.CLASS_NAME, value=photo_btn_class)
+    photo_btn.click()
+
+    # Click on frames
+    frames_parent_class = "imgedit-profile-photo-frame-viewer__actions"
+    frames_parent = driver.find_element(by=By.CLASS_NAME, value=frames_parent_class)
+    frame_btn = frames_parent.find_element(by=By.XPATH, value="./button[position()=3]")
+    frame_btn.click()
+
+    # Click on OpenToWork frame
+    original_frame_btn_xpath = "//ul[contains(@class, 'imgedit-profile-frame-selector__frames')]/li[position()=2]/button"
+    original_frame_btn = driver.find_element(
+        by=By.XPATH, value=original_frame_btn_xpath
+    )
+    original_frame_btn.click()
+
+    # Apply btn
+    apply_btn_xpath = (
+        "//div[contains(@class, 'imgedit-profile-frame-selector__actions')]/button"
+    )
+    apply_btn = driver.find_element(by=By.XPATH, value=apply_btn_xpath)
+    apply_btn.click()
+
+    # Location type
+    loc_type_btns_xpath = "//fieldset[contains(@id, 'pill-form-component-openToWorkPreferencesFormElement-WORKPLACES')]/button"
+    loc_type_btns = driver.find_elements(by=By.XPATH, value=loc_type_btns_xpath)
+    for button in loc_type_btns:
+        if "artdeco-pill--selected" not in button.get_attribute("outerHTML"):
+            button.click()
+
+    # Find and add locations on site
+    LOCATIONS = ["London", "Royston"]
+
+    for location in LOCATIONS:
+        # When selecting remote location in the previous step, a new section is added to the form and that takes few miliseconds.
+        # Similarly happens when a new location is added - it is displayed after few miliseconds.
+        # Then the following sleep is required to prevent the location textbox from disappearing due to the delayed HTML updates.
+        time.sleep(1)
+        add_loc_btns_xpath = "//fieldset[contains(@id, 'pill-form-component-openToWorkPreferencesFormElement-JOB-LOCATIONS')]/div/button"
+        loc_on_site = driver.find_element(by=By.XPATH, value=add_loc_btns_xpath)
+        loc_on_site.click()
+        location_class = "typeahead-cta__input"
+        location_input = driver.find_element(by=By.CLASS_NAME, value=location_class)
+        location_input.send_keys(location)
+        # The following sleep is necessary for LinkedIn to fill the locations matching with the inserted one.
+        time.sleep(3)
+        location_input.send_keys(Keys.DOWN)
+        location_input.send_keys(Keys.ENTER)
+    # The following sleep is there for the same reason as the first sleep in the above for loop.
+    time.sleep(1)
+
+    # Start date: inmediatly available
+    start_btns_xpath = "//fieldset[contains(@id, 'radio-button-form-component-openToWorkPreferencesFormElement-START-DATE')]/div[position()=1]/label"
+    start_btn = driver.find_element(by=By.XPATH, value=start_btns_xpath)
+    start_btn.click()
+
+    # Employment type
+    employment_type_btns_xpath = "//fieldset[contains(@id, 'pill-form-component-openToWorkPreferencesFormElement-JOB-TYPES')]/button"
+    employment_type_btns = driver.find_elements(
+        by=By.XPATH, value=employment_type_btns_xpath
+    )
+    for button in employment_type_btns:
+        if "artdeco-pill--selected" not in button.get_attribute("outerHTML"):
+            button.click()
+
+    # Visibility: All LinkedIn members
+    visibility_btns_xpath = "//fieldset[contains(@id, 'radio-button-form-component-openToWorkPreferencesFormElement-VISIBILITY')]/div[position()=2]/label"
+    visibility_btn = driver.find_element(by=By.XPATH, value=visibility_btns_xpath)
+    visibility_btn.click()
+
+    # Save
+    save_btn_xpath = "//div[contains(@id, 'artdeco-modal-outlet')]//button"
+    save_btn = driver.find_elements(by=By.XPATH, value=save_btn_xpath)[-1]
+    save_btn.click()
 
 
 def close_status(driver):
+    """This function will turn the OpenToWork status off, removing the frame
+
+    Args:
+        driver (WebDriver): the driver instanced to be used in other function.
+    """
+    # Go to the profile page
     driver.get("https://www.linkedin.com/in/me/")
 
     # Open the image menu
@@ -108,11 +214,11 @@ def close_status(driver):
     checkbox_input.click()
 
     # Submit
-    save_btn_xpath = (
+    submit_btn_xpath = (
         "//footer[contains(@class, 'remove-frame-modal__actions')]/button[position()=2]"
     )
-    save_btn = driver.find_element(by=By.XPATH, value=save_btn_xpath)
-    save_btn.click()
+    submit_btn = driver.find_element(by=By.XPATH, value=submit_btn_xpath)
+    submit_btn.click()
 
 
 def parse_args():
@@ -121,14 +227,59 @@ def parse_args():
             "This script is intended to access the LinkedIn status and check it and change it based on the day of the week."
         )
     )
+    params.add_argument(
+        "--open",
+        action="store_true",
+        help=(
+            "If provided, the script will select the open to work frame in LinkedIn."
+        ),
+    )
+    params.add_argument(
+        "--close",
+        action="store_true",
+        help=("The script will select the original frame in LinkedIn."),
+    )
+
+    args = params.parse_args()
+
+    if args.open and args.close:
+        error_message = (
+            "Both args.open and args.close are True, this is not a valid combination."
+        )
+        raise ValueError(error_message)
+
+    if (args.open == False) and (args.close == False):
+        error_message = (
+            "Both args.open and args.close are False, this is not a valid combination."
+        )
+        raise ValueError(error_message)
+
+    return args
 
 
 if __name__ == "__main__":
+    args = parse_args()
+
     driver = open_driver()
+    driver.implicitly_wait(10)
     secrets = load_secrets()
     username = secrets["email"]
     password = secrets["pass"]
     login_linkedin(driver, username, password)
     is_open = is_work_status_open(driver)
-    close_status(driver)
-    # close_driver(driver)
+
+    if args.open:
+        if is_open:
+            print("The LinkedIn account is alredy open. Aborting mission...")
+            close_driver(driver)
+        else:
+            open_status(driver)
+            close_driver(driver)
+
+    if args.close:
+        if not is_open:
+            print("The LinkedIn account is alredy closed. Aborting mission...")
+            close_driver(driver)
+        else:
+            close_status(driver)
+            close_driver(driver)
